c96ff22b95f132e73a02d587bedbb488ff2e3688
nikozdev <nikozdev@nikozdev.net>
2023-08-26 11:00:16 -0700

    !main!v0.0!init!test!build!system!setup!structure!file!code!envi!docs!make
    [.gitattributes] > simple git file to resolve CR;LF on different systems
    [.gitignore] > simple git file to exclude .DS_Store from version control
    [docs/] > directory for informational files like readme and license
    [docs/Readme.md] > added a simple file with general, build and testing info
    [docs/License.md] > added a simple MIT license file with "nikozdev" copyright
    [envi/] > directory for environmental files like tool scripts and configs
    [envi/clang-format.yaml] > added the latest clang-format config
    [envi/CMakeLists.txt] > added a generic cmake build script
    [code/] > directory for source code files
    [code/GameOfLife.cpp] > added an overengineered generic source code file
    [code/GameOfLife.hpp] > added a header file for precompilation of huge headers
    [make/] > directory for generated meta files
    [make/.gitignore] > added a simple git file to ignore all generated meta files

---
+++ code/GameOfLife.hpp
@@ -0,0 +1,11 @@
+#ifndef dGameOfLife_Hpp
+#define dGameOfLife_Hpp
+//headers
+#include <cstdlib>
+#include <string_view>
+#include <string>
+#include <unordered_map>
+#include <iostream>
+#include <functional>
+//content
+#endif//dGameOfLife_Hpp
