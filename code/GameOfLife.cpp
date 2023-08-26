f0593e9cc10c093f5fc1ab4ce55254cb55c0a62f
nikozdev <nikozdev@nikozdev.net>
2023-08-26 12:55:46 -0700

    !work!code!envi!test!libs!deps!extend!include!sfml!system!graphics!api
    [code/GameOfLife.hpp] > include sfml system and graphics headers
    [code/GameOfLife.cpp] > added test for sfml window
    [envi/CMakeLists.txt] > added sfml system and graphics libraries

--- code/GameOfLife.cpp
+++ code/GameOfLife.cpp
@@ -12,14 +12,14 @@ using tTestFun = std::function<tTestOut(void)>;
 using tTestTab = std::unordered_map<tTestKey, tTestFun>;
 using tTestRef = tTestTab::iterator;
 //actions
-int main(int vArgC, char **vArgV, char **vEnvi)
+int main(int vArgC, char *vArgV[], char *vEnvi[])
 {
 	extern tTestTab vTestTab;
 	if(vArgC == 3 && std::string_view(vArgV[1]) == "test")
 	{
-    tTestKey vTestKey = vArgV[2];
-    tTestRef vTestRef = vTestTab.find(vTestKey);
-    return (vTestRef == vTestTab.end()) ? EXIT_FAILURE : vTestRef->second();
+		tTestKey vTestKey = vArgV[2];
+		tTestRef vTestRef = vTestTab.find(vTestKey);
+		return (vTestRef == vTestTab.end()) ? EXIT_FAILURE : vTestRef->second();
 	}
 	else
 	{
@@ -33,8 +33,54 @@ tTestTab vTestTab = {
 	{"Hello",
 	 []()
 	 {
-     std::cout << "HelloWorld" << std::endl;
+		 std::cout << "HelloWorld" << std::endl;
 		 return EXIT_SUCCESS;
-	 }}
+	 }},
+	{"SfmlWindow",
+	 []()
+	 {
+		 auto vVideoMode = sf::VideoMode{
+			 0x400,//width
+			 0x400,//height
+			 0x20, //bits per pixel
+		 };
+		 auto vContextSettings = sf::ContextSettings{
+			 0,																			 //depthBits
+			 0,																			 //stencil
+			 0,																			 //antialiasingLevel
+			 1,																			 //major
+			 1,																			 //minor
+			 sf::ContextSettings::Attribute::Default,//attributes
+			 false,																	 //sRgb
+		 };
+		 auto vWindowStyle = sf::Uint32(sf::Style::None);
+		 vWindowStyle |= sf::Style::Resize;
+		 vWindowStyle |= sf::Style::Close;
+		 vWindowStyle |= sf::Style::Titlebar;
+		 auto vWindow = sf::Window{
+			 vVideoMode,
+			 "GameOfLife",
+			 vWindowStyle,
+			 vContextSettings,
+		 };
+		 while(vWindow.isOpen())
+		 {
+			 sf::Event vEvent;
+			 while(vWindow.pollEvent(vEvent))
+			 {
+				 switch(vEvent.type)
+				 {
+				 case sf::Event::Closed:
+				 {
+					 vWindow.close();
+				 }
+				 break;
+				 default: break;
+				 }
+			 }
+		 }
+		 std::cout << "HelloWindow" << std::endl;
+		 return EXIT_SUCCESS;
+	 }},
 };
 #endif//dGameOfLife_Cpp
--- code/GameOfLife.hpp
+++ code/GameOfLife.hpp
@@ -1,11 +1,15 @@
 #ifndef dGameOfLife_Hpp
 #define dGameOfLife_Hpp
 //headers
+//-//standard
 #include <cstdlib>
 #include <string_view>
 #include <string>
 #include <unordered_map>
 #include <iostream>
 #include <functional>
+//-//external
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
 //content
 #endif//dGameOfLife_Hpp
